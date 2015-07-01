27783787cce995ab4e8d73df02eaf3717cf20bbe
Joe Wright <joe@joejag.com>
2015-07-01 21:45:37 +0100

    Github clone helpers

--- joejag.plugin.zsh
+++ joejag.plugin.zsh
@@ -3,3 +3,16 @@ function webize() {
   filename=$(basename $1)
   convert $1 -resize "600x" -unsharp 2x0.5+0.7+0 -quality 98 ${filename%%.*}_web.jpg;
 }
+
+# Clone all an individuals projects on Github
+function github_clone_by_user() {
+  username=$1
+  curl https://api.github.com/users/$username/repos | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
+}
+
+# Clone all an organisations projects onn Github
+function github_clone_by_org() {
+  org=$1
+  curl https://api.github.com/orgs/$org/repos| ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
+}
+
